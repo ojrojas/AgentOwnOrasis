28dcd80f9315f2e659e815f54585121f664fcb71
Oscar Julian Rojas <orojasgarces1@gmail.com>
2025-05-15 12:34:19 -0500

    update settins extension, ollama url base ollama and template

---
+++ oroasis/src/providers/core/services/ollama.service.ts
@@ -0,0 +1,25 @@
+import { GenerateRequest, Ollama } from 'ollama';
+import * as vscode from 'vscode';
+
+export class OllamaApiService {
+    readonly ollama: Ollama;
+    templateGenerate: string | undefined;
+    constructor() {
+        const settings = vscode.workspace.getConfiguration("oroasisSettings");
+        this.templateGenerate = settings.get('templatePromptGenerate');
+        this.ollama = new Ollama({ host: settings.get('ollamaBaseUrl') });
+    }
+
+    listModels = () => {
+        return this.ollama.list();
+    };
+
+    generate = (request: GenerateRequest) => {
+        return this.ollama.generate({
+            model: request.model,
+            prompt: request.prompt,
+            template: this.templateGenerate
+        });
+    };
+
+}
